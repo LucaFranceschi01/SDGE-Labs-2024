2ac74ea2c35d242f340758dc67508de66fca3c83
alejandrogonzalez14 <alex41596@gmail.com>
2024-01-27 19:24:53 +0100

    idk if this is correct, but i tried

--- lab1/src/main/java/edu/upf/parser/SimplifiedTweet.java
+++ lab1/src/main/java/edu/upf/parser/SimplifiedTweet.java
@@ -2,25 +2,29 @@ package edu.upf.parser;
 
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
+import com.google.gson.*;
+
 public class SimplifiedTweet {
 
   // All classes use the same instance
   private static JsonParser parser = new JsonParser();
 
-
-
   private final long tweetId;			  // the id of the tweet ('id')
   private final String text;  		      // the content of the tweet ('text')
-  private final long ;			  // the user id ('user->id')
+  private final long userId;			  // the user id ('user->id')
   private final String userName;		  // the user name ('user'->'name')
   private final String language;          // the language of a tweet ('lang')
   private final long timestampMs;		  // seconduserIds from epoch ('timestamp_ms')
 
-  public SimplifiedTweet(long tweetId, String text, long userId, String userName,
-                         String language, long timestampMs) {
-
-    // PLACE YOUR CODE HERE!
-
+  public SimplifiedTweet(long tweetId, String text, long userId, String userName, String language, long timestampMs) {
+    this.tweetId = tweetId;
+    this.text = text;
+    this.userId = userId;
+    this.userName = userName;
+    this.language = language;
+    this.timestampMs = timestampMs;
   }
 
   /**
@@ -33,12 +37,32 @@ public class SimplifiedTweet {
   public static Optional<SimplifiedTweet> fromJson(String jsonStr) {
 
     // PLACE YOUR CODE HERE!
+    
+    try {
+      JsonElement jsonElement = JsonParser.parseString(jsonStr);
+      JsonObject jsonObject = jsonElement.getAsJsonObject();
 
+      long tweetId = jsonObject.get("id").getAsLong();
+      String text = jsonObject.get("text").getAsString();
+      long userId = jsonObject.getAsJsonObject().get("id").getAsLong();
+      String userName = jsonObject.getAsJsonObject().get("name").getAsString();
+      String language = jsonObject.get("lang").getAsString();
+      long timestampMs = jsonObject.get("timestamp_ms").getAsLong();
+
+      SimplifiedTweet tweet = new SimplifiedTweet(tweetId, text, userId, userName, language, timestampMs);
+      Optional<SimplifiedTweet> opt_tweet = Optional.of(tweet); // no estoy seguro pq no se si checkea si hay valores vacios en 'tweet'
+      return opt_tweet;
+
+    }
+    catch (Exception e) {
+      Optional<SimplifiedTweet> empty = Optional.empty();
+      return empty;
+    }
   }
 
 
   @Override
   public String toString() {
-    return "";
+    return ""; // esto no se si hay q cambiarlo pq en el documento del enunciado tienen algo diferente??
   }
 }
