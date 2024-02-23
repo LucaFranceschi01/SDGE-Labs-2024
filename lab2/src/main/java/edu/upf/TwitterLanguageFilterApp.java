b1558c54548f4d4e59cac537d723b4c0e1d15f49
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2024-02-23 09:35:05 +0100

    no consigo ejecutar

--- lab2/src/main/java/edu/upf/TwitterLanguageFilterApp.java
+++ lab2/src/main/java/edu/upf/TwitterLanguageFilterApp.java
@@ -1,5 +1,25 @@
 package edu.upf;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class TwitterLanguageFilterApp {
-    // SERÁ IGUAL QUE LAB1 TWITTERFILTER.JAVA
+    public static void main( String[] args ){
+        List<String> argsList = Arrays.asList(args);
+        String language = argsList.get(0);
+        String outputFile = argsList.get(1);
+        String bucket = argsList.get(2);
+
+        System.out.println(language);
+        System.out.println(outputFile);
+        System.out.println(bucket);
+
+        //Create a SparkContext to initialize
+        SparkConf conf = new SparkConf().setAppName("Twitter Filter");
+        JavaSparkContext sparkContext = new JavaSparkContext(conf);
+        sparkContext.close();
+    }
 }
