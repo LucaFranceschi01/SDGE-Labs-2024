28de4b83021aec886943c3b3fcbf71f136cd3fb6
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2024-03-03 16:27:41 +0100

    avances creo

--- lab2/src/main/java/edu/upf/MostRetweetedApp.java
+++ lab2/src/main/java/edu/upf/MostRetweetedApp.java
@@ -22,68 +22,74 @@ public class MostRetweetedApp {
         //Create a SparkContext to initialize
         SparkConf conf = new SparkConf().setAppName("MostRetweetedApp");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
+        JavaRDD<String> tweets = sparkContext.emptyRDD();
 
+        // Load input
         for(String inputFile: argsList.subList(1, argsList.size())) {
-            // Load input
-            JavaRDD<String> tweets = sparkContext.textFile(inputFile);
-
-            // Parsed tweets (non-empty and retweeted ones)
-            JavaRDD<Optional<ExtendedSimplifiedTweet>> filteredTweets = tweets
-                .map(ExtendedSimplifiedTweet::fromJson)
-                .filter(tweet -> !tweet.isEmpty())
-                .filter(tweet -> tweet.get().getIsRetweeted());
-
-            // Retweeted users count
-            JavaPairRDD<Long, Long> retweeted_users_count = filteredTweets
-                .map(tweet -> tweet.get().getRetweetedUserId())
-                .mapToPair(user_id -> new Tuple2<Long, Long>(user_id, 1L))
-                .reduceByKey((a, b) -> a + b);
-
-            // Sort descending (most retweeted users first)
-            JavaPairRDD<Long, Long> retweeted_users_count_descending = retweeted_users_count
-                .mapToPair(tuple -> new Tuple2<Long, Long>(tuple._2, tuple._1))
-                .sortByKey(false)
-                .mapToPair(tuples -> new Tuple2<Long, Long>(tuples._2, tuples._1));
-
-            // Retweeted tweets count
-            JavaPairRDD<Long, Long> retweeted_tweets_count = filteredTweets
-                .map(tweet -> tweet.get().getRetweetedId())
-                .mapToPair(tweet_id -> new Tuple2<Long, Long>(tweet_id, 1L))
-                .reduceByKey((a, b) -> a + b);
-
-            // Sort descending (most retweeted tweets first)
-            JavaPairRDD<Long, Long> retweeted_tweets_count_descending = retweeted_tweets_count
-                .mapToPair(tuple -> new Tuple2<Long, Long>(tuple._2, tuple._1))
-                .sortByKey(false)
-                .mapToPair(tuples -> new Tuple2<Long, Long>(tuples._2, tuples._1));
-
-            // Retrieve top ten users (list format)
-            List<Long> top_ten_users = retweeted_users_count_descending
-                .map(user_and_count -> user_and_count._1)
-                .take(10);
-
-            // Calculate most retweeted tweet for each of the top ten most retweeted users.
-            // for (Long user : top_ten_users) {
-            //     JavaPairRDD<Long, Long> most_retweeted_tweet_user = retweeted_users_count
-            //         .filter(tweet -> filterTweetsByRetweetedUser(tweet._1, user));
-            // }
-
+            tweets = sparkContext.textFile(inputFile).union(tweets);
         }
-        
+
+        // Parsed tweets (non-empty and retweeted ones)
+        JavaRDD<Optional<ExtendedSimplifiedTweet>> filteredTweets = tweets
+            .map(ExtendedSimplifiedTweet::fromJson)
+            .filter(tweet -> !tweet.isEmpty())
+            .filter(tweet -> tweet.get().getIsRetweeted());
+
+        // Retweeted users count
+        JavaPairRDD<Long, Long> retweeted_users_count = filteredTweets
+            .map(tweet -> tweet.get().getRetweetedUserId())
+            .mapToPair(user_id -> new Tuple2<Long, Long>(user_id, 1L))
+            .reduceByKey((a, b) -> a + b);
+
+        // Sort descending (most retweeted users first)
+        JavaPairRDD<Long, Long> retweeted_users_count_descending = retweeted_users_count
+            // .mapToPair(tuple -> new Tuple2<Long, Long>(tuple._2, tuple._1))
+            .mapToPair(tuple -> tuple.swap())
+            .sortByKey(false)
+            .mapToPair(tuple -> tuple.swap());
+            // .mapToPair(tuples -> new Tuple2<Long, Long>(tuples._2, tuples._1));
+
+        // Retrieve top ten users (list format)
+        List<Long> top_ten_users = retweeted_users_count_descending
+            .map(user_and_count -> user_and_count._1)
+            .take(10);
+
+        // Retweeted tweets count ((retweetID, authorUserID), count)
+        JavaPairRDD<Tuple2<Long, Long>, Long> retweeted_tweets_count = filteredTweets
+            .mapToPair(tweet -> new Tuple2<Tuple2<Long, Long>, Long>(
+                new Tuple2<Long, Long>(tweet.get().getRetweetedId(), tweet.get().getRetweetedUserId()),
+                1L)
+            )
+            .reduceByKey((a, b) -> a + b);
+
+        // Sort descending (most retweeted tweets first)
+        JavaPairRDD<Tuple2<Long, Long>, Long> retweeted_tweets_count_descending = retweeted_tweets_count
+            // .mapToPair(tuple -> new Tuple2<Long, Long>(tuple._2, tuple._1))
+            .mapToPair(tuple -> tuple.swap())
+            .sortByKey(false)
+            .mapToPair(tuple -> tuple.swap());
+            // .mapToPair(tuples -> new Tuple2<Long, Long>(tuples._2, tuples._1));
+
+        JavaPairRDD<Long, Long> last = retweeted_tweets_count_descending // FALTA HACER QUE SOLO HAYA UN TWEET CON MAX RETWEETS PER BEST USER
+            .filter(entry -> top_ten_users.contains(entry._1._2))
+            .mapToPair(entry -> entry._1);
+
+        last.saveAsTextFile(outputDir);
+
         sparkContext.close();
     }
 
-    public static boolean filterTweetsByRetweetedUser(Optional<ExtendedSimplifiedTweet> tweet, Long retweeted_user) {
-        // Get the retweeted user from the tweet
-        Long tweet_retweeted_user = tweet.get().getRetweetedUserId();
-        Long retweeted_tweet_id = tweet.get().getRetweetedId();
-        // Check if the tweet has been retweeted by the user
-        if (retweeted_user.equals(tweet_retweeted_user)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // public static boolean filterTweetsByRetweetedUser(Optional<ExtendedSimplifiedTweet> tweet, Long retweeted_user) {
+    //     // Get the retweeted user from the tweet
+    //     Long tweet_retweeted_user = tweet.get().getRetweetedUserId();
+    //     Long retweeted_tweet_id = tweet.get().getRetweetedId();
+    //     // Check if the tweet has been retweeted by the user
+    //     if (retweeted_user.equals(tweet_retweeted_user)) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     /*
     public static boolean fiterTweetsByRetweetedUser(Optional<ExtendedSimplifiedTweet> tweet, List<Long> retweeted_users) {
