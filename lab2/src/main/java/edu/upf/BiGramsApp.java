d96ff6b5a3dbac1522bc6b1e937e83868a13dc43
alejandrogonzalez14 <alex41596@gmail.com>
2024-03-04 17:27:31 +0100

    empty strings fix

--- lab2/src/main/java/edu/upf/BiGramsApp.java
+++ lab2/src/main/java/edu/upf/BiGramsApp.java
@@ -55,7 +55,7 @@ public class BiGramsApp {
     }
 
     private static String normalise(String word) {
-        return word.trim().toLowerCase(); //Remove word trimming, lower-casing
+        return word.trim().toLowerCase(); //Word trimming, lower-casing
     }
 
     private static List<Tuple2<String, String>> bigramsFromText(String text) {
@@ -63,19 +63,13 @@ public class BiGramsApp {
         List<Tuple2<String, String>> bigrams = new ArrayList<Tuple2<String, String>>();
         
         // Get array of words
-        String[] words = text.split(" ");
-
-        // Remove the elements equal to " "                             // --> no se si lo tiene q hacer el normalise, yo creo q se puede dejar ahi y explicar en el report
-        List<String> filteredWords = new ArrayList<String>();
-        for (String word:words) {
-            if (!word.equals(" ")) {
-                filteredWords.add(word);
-            }
-        }
+        List<String> words = Arrays.asList(text.split(" "));
 
         // Create bigrams (normalising first) and adding them to the bigram list
-        for (int ind = 0; ind < words.length - 2; ind++) {
-            bigrams.add(new Tuple2<String,String>(normalise(filteredWords.get(ind)), normalise(filteredWords.get(ind+1))));
+        for (int ind = 0; ind < words.size() - 2; ind++) {
+            if (!normalise(words.get(ind)).isEmpty() && !normalise(words.get(ind+1)).isEmpty()) {
+                bigrams.add(new Tuple2<String,String>(normalise(words.get(ind)), normalise(words.get(ind+1))));
+            }
         }
 
         return bigrams;
