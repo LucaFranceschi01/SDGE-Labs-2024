0b165fe89687732ed36f3136d8da5151669e2df3
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2024-03-07 11:49:52 +0100

    remove import tonto

--- lab3/src/main/java/edu/upf/MastodonStateless.java
+++ lab3/src/main/java/edu/upf/MastodonStateless.java
@@ -1,7 +1,5 @@
 package edu.upf;
 
-import javax.naming.event.ObjectChangeListener;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
